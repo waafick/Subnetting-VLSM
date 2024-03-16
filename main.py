f66d1d23716889f11e3d11b351235a6d562dbbb0
Wafick <129459713+waafick@users.noreply.github.com>
2024-03-16 23:23:49 +0000

    Update

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-def ip_check() -> list :
-    while True :
-        ip = input("Ip address in format x.x.x.x : ")
-        ip = ip.split('.')
+from sys import exit
 
+
+
+def ip_check() -> list  :
+
+    while True :
+        ip = input("Ip address format x.x.x.x : ").split(".")
         if len(ip) != 4 :
             print("Invalid ip address")
         else :
-            return ip
+            for numbers in ip :
+                if int(numbers) in range(0,256) :
+                    return(list(ip))
+                
     
-   
-
-    
-
 
 print(ip_check())
